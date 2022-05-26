5a1542e87e496822e24360185e6d2053337d1f0a
HuynhBuiNgocNhu <nhuhbngcc19171@fpt.edu.vn>
2022-05-26 13:50:28 +0700

    first commit

---
+++ conection.php
@@ -0,0 +1,4 @@
+<?php
+$conn = pg_connect("postgres://zgdarvsmxreovg:6a6243b0883ac947ea765f5820835d40febf355e064b3ba3fc2319858f524d58@ec2-54-204-56-171.compute-1.amazonaws.com:5432/d3id24985v1vvn")
+or die ("can not connect database".pg_connect_error());
+?>
